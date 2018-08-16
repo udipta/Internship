5414ef19fae5bc598cac0a90c177e3abf279fac4
udipta <iamudi03@gmail.com>
2018-08-16 19:28:16 +0530

    upload work

---
+++ unity files/sample/Assets/movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(0, 500, 100);
+		
+	}
+}
---
+++ unity files/sample/Assets/script.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class script : MonoBehaviour {
+
+	public Rigidbody rb;
+	// Use this for initialization
+	
+	void FixedUpdate () {
+		
+		if(Input.GetKey("d"))
+			rb.AddForce(10 , 2, 2);
+
+		if(Input.GetKey("a"))
+			rb.AddForce(-10, -1, -1);
+		
+		if(Input.GetKey("w"))
+			rb.AddForce(1, 1, 10);
+		
+		if(Input.GetKey("s"))
+			rb.AddForce(-1, -1, -10);
+	}
+}
